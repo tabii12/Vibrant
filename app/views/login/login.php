3ef25ad21d3c71e31c44dfce3fac465dc62d3199
bear080604 <phuonganhkhanh683@gmail.com>
2024-11-28 00:35:30 +0700

    sửa, thêm cgi viết vào đây

--- app/views/login/login.php
+++ app/views/login/login.php
@@ -35,7 +35,7 @@
                 </div>
             </div>
             <div class="login_button">
-                <input type="submit" name="submit" id="submit" value=>
+                <input type="submit" name="submit" id="submit" value="Đăng nhập">
             </div>
           </form>
             <div class="go-to_signup">
