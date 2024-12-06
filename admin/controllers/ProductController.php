baace92bb5a3e4ae98c25c3f395c0735a9c3660f
JePpydeyy <tisz1612005@gmail.com>
2024-12-06 20:24:20 +0700

    xoa san pham

--- admin/controllers/ProductController.php
+++ admin/controllers/ProductController.php
@@ -14,6 +14,18 @@
             $this->data['product_info'] = $this->product->getProducts();
 
             $this->RenderProduct($this->data);
+
+           
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+                $this->product->deleteProductImages($id);
+                $this->product->deleteProductById($id);
+        
+                // Sau khi xóa xong, điều hướng về trang danh sách sản phẩm
+                header('Location: index.php?page=product');
+                exit();
+            }
+            
         }
     }
 ?>
