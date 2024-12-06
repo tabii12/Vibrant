7038d1befecdb3b9989e15cadc5595727852ad24
JePpydeyy <tisz1612005@gmail.com>
2024-12-07 01:14:42 +0700

    xoa nguoi dung

--- admin/models/KhachHangModel.php
+++ admin/models/KhachHangModel.php
@@ -17,8 +17,18 @@
         }
 
         public function deleteCustomer($id_khach_hang) {
-            $sql = "DELETE FROM nguoidung WHERE id = ?";
-            return $this->db->query($sql, [$id_khach_hang]);
+            $sql_delete_ct_donhang = "DELETE FROM ct_donhang 
+            WHERE id_don_hang IN 
+            (SELECT id FROM donhang WHERE id_nguoi_dung = ?)";
+            $this->db->query($sql_delete_ct_donhang, [$id_khach_hang]);
+
+            
+            $sql_delete_orders = "DELETE FROM donhang WHERE id_nguoi_dung = ?";
+            $this->db->query($sql_delete_orders, [$id_khach_hang]);
+
+            
+            $sql_delete_customer = "DELETE FROM nguoidung WHERE id = ?";
+            return $this->db->query($sql_delete_customer, [$id_khach_hang]);
         }
     }
 
